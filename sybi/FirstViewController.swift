ada8b3daa63a1a7084373a452a5a084d948e9124
fernandoybus <fernando.azevedo@gmail.com>
2015-08-16 00:33:42 -0400

    Initial Commit

---
+++ sybi/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  sybi
+//
+//  Created by Gisele Sardas on 8/16/15.
+//  Copyright (c) 2015 ybus. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
